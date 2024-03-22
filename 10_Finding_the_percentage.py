b5c57f8f5fbdcf98fa16320e4654d74b21670fea
Aashish Bedi <120207125+AashishBedi@users.noreply.github.com>
2024-03-22 16:37:20 +0530

    Add files via upload

---
+++ 10_Finding_the_percentage.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    
+    
+l1 = list(student_marks[query_name])
+avg = sum(l1)/len(l1)
+print('%.2f'% avg)
